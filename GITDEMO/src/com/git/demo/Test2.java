c7f59dab343de472c352fc4eaea9b194ca62f555
tl <swami.vikas04@gmail.com>
2020-04-19 15:00:14 -0400

    added m1 method in Test2.java

--- GITDEMO/src/com/git/demo/Test2.java
+++ GITDEMO/src/com/git/demo/Test2.java
@@ -4,4 +4,11 @@ public class Test2 {
 public static void main(String[] args) {
 	System.out.println("TL copyupdated1 ");
 }
+
+
+public static void m1 () {
+	System.out.println(" Test2 m1 method added  by dev");
+}
+
+
 }
