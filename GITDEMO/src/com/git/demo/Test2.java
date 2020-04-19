e8eb1f264f31d62ae4d0a9476eb3e1aa987463f0
tl <swami.vikas04@gmail.com>
2020-04-19 15:01:47 -0400

    updated m1

--- GITDEMO/src/com/git/demo/Test2.java
+++ GITDEMO/src/com/git/demo/Test2.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 
 
 public static void m1 () {
-	System.out.println(" Test2 m1 method added  by dev");
+	System.out.println(" Test2 m1 method added  by dev : 1234");
 }
 
 
