817bc5afdda705fde3a6cb2300a682ff3d8c956a
tl <swami.vikas04@gmail.com>
2020-04-19 21:33:10 -0400

    test

---
+++ GITDEMO/src/com/git/demo/CopyOfF1.java
@@ -0,0 +1,19 @@
+ 
+package com.git.demo;
+
+public class CopyOfF1 {
+
+	public static void main(String[] args) {
+		 
+		System.out.println("TL updated");
+ 
+		
+ 
+	}
+
+ 
+		 System.out.println("git pull --all);
+	}
+
+ 
+}
