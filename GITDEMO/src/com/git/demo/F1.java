f78bdf6433b2f5a46687ee4e291917e845e2a197
vikasnikuu04 <45723524+vikasnikuu04@users.noreply.github.com>
2020-04-19 20:13:03 -0400

    Update F1.java

--- GITDEMO/src/com/git/demo/F1.java
+++ GITDEMO/src/com/git/demo/F1.java
@@ -7,7 +7,7 @@ public class F1 {
 		 
 		System.out.println("TL updated");
  
-	System.out.println("git pull origin master  - no wbranch otap");
+	System.out.println("git pull origin master);
 		
  
 	}
